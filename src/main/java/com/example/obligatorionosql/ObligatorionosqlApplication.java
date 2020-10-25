d82360ca4f0219b1736f9ccf2f865f671781e0c5
Gonzalo Santa María <gonzalo.santa.maria@fing.edu.uy>
2020-10-25 03:38:27 -0300

    Configuraciones varias
    Quitar basic-error-controller del Swagger
    Mostrar siempre mensaje de error

--- src/main/java/com/example/obligatorionosql/ObligatorionosqlApplication.java
+++ src/main/java/com/example/obligatorionosql/ObligatorionosqlApplication.java
@@ -1,15 +1,11 @@
 package com.example.obligatorionosql;
 
-import com.mongodb.MongoClient;
 import dev.morphia.Morphia;
 import models.Comentario;
 import models.Emocion;
 import models.Usuario;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class ObligatorionosqlApplication {
